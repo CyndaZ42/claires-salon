83522f0c1a53aa47d39fbc452a7e1b0749c895c3
Zachary Waggoner <zacwaggoner42@gmail.com>
2022-07-29 17:07:13 -0700

    add create view to client

--- HairSalon/Controllers/ClientController.cs
+++ HairSalon/Controllers/ClientController.cs
@@ -22,5 +22,55 @@ namespace HairSalon.Controllers
       ViewBag.PageTitle = "View All Clients";
       return View(model);
     }
+
+    public ActionResult Create()
+    {
+      ViewBag.ClientId = new SelectList(_db.Client, "ClientId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Stylist stylist)
+    {
+      _db.Stylist.Add(stylist);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      Stylist thisStylist = _db.Stylist.FirstOrDefault(stylist => stylist.StylistId == id);
+      return View(thisStylist);
+    }
+    
+    public ActionResult Edit(int id)
+    {
+      var thisStylist = _db.Stylist.FirstOrDefault(stylist => stylist.StylistId == id);
+      ViewBag.ClientId = new SelectList(_db.Client, "ClientId", "Name");
+      return View(thisStylist);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Stylist stylist)
+    {
+      _db.Entry(stylist).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var thisStylist = _db.Stylist.FirstOrDefault(stylist => stylist.StylistId == id);
+      return View(thisStylist);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisStylist = _db.Stylist.FirstOrDefault(stylist => stylist.StylistId == id);
+      _db.Stylist.Remove(thisStylist);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
